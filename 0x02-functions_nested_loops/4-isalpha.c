1246ac159dc4eed251892b63380b5576c1510cac
KundaiMH <kundai.m.conte@gmail.com>
2023-06-24 02:52:09 -0700

    tried to fix errors

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * _isalpha - checks if character is a letter
@@ -6,15 +6,12 @@
  *
  * @c: takes input from other functions.
  *
- * Return: 1 is c if true else 0
+ * Return: 1 if c is a letter, else 0
 */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	return (0);
 }
-
-
