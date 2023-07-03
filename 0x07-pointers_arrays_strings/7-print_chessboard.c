61b37133e0acb89f0f2bb177b0dc36183133166e
KundaiMH <kundai.m.conte@gmail.com>
2023-07-03 05:30:02 -0700

    a function that prints the chessboard.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,27 +3,25 @@
 /**
  * print_chessboard - prints the chess board
  *
- * @a: input pointer
+ * @a: input pointer to 2D array representing the chessboard
  *
- * Return: return nothing
-*/
+ * Return: void
+ */
 
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
 	i = 0;
-	j = 0;
-	while (i < 64)
+	while (i < 8)
 	{
-		if (i % 8 == 0 && i != 0)
+		j = 0;
+		while (j < 8)
 		{
-			j = i;
-			_putchar('\n');
+			_putchar(a[i][j]);
+			j++;
 		}
-
-		_putchar(a[i / 8][i - j]);
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
