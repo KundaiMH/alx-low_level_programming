b9d5cd67c07e6c06aad121c6631a1150a9fcdd45
KundaiMH <kundai.m.conte@gmail.com>
2023-07-05 16:08:24 -0700

    a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,45 @@
 /**
- * _sqrt_recursion_wrapper - A wrapper that performs the recursion for square root calculation
- * @n: The input number
- * @min: The minimum number to guess
- * @max: The maximum number to guess
+ * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
  *
- * Return: The square root of @n, or -1 if no perfect square exists
- */
+ * @n : input number
+ * @min: minimum number to guess
+ * @max: maximum number to guess
+ *
+ * Return: square root of @n or -1
+*/
 int _sqrt_recursion_wrapper(int n, int min, int max)
 {
-	int guess;
+	int guess, guess_squared;
 
-	guess = (min + max) / 2; /* Get the guess (g) */
+	guess = (min + max) / 2;		/*get guess (g)*/
+	guess_squared = guess * guess;		/*square guess (sg)*/
 
-	if (guess * guess == n) /* If squared guess is equal to n, return guess */
-		return guess;
-	else if (min + 1 == max && guess * guess < n) /* Check if consecutive numbers and guess_squared < n */
-		return -1;
-	else if (guess * guess < n) /* Overshoot the guess if sg is less than n */
-		return _sqrt_recursion_wrapper(n, guess + 1, max);
-	else /* Undershoot the guess if sg is greater than n */
-		return _sqrt_recursion_wrapper(n, min, guess);
+	if (guess_squared == n)			/*if squared guess == n return guess*/
+		return (guess);
+	else if (min == max)			/*this means n doesn't have a perfect square*/
+		return (-1);
+	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
+		return (_sqrt_recursion_wrapper(n, guess + 1, max));
+	else					/*undershoot g if sg is more than the n*/
+		return (_sqrt_recursion_wrapper(n, min, guess - 1));
 }
 
 /**
- * _sqrt_recursion - A function that returns the natural square root of a number
- * @n: The input number
+ * _sqrt_recursion - a function that returns the natural
+ *                   square root of a number
  *
- * Return: The square root of @n
- */
+ * @n: input number
+ *
+ * Return: square root
+*/
 int _sqrt_recursion(int n)
 {
-	if (n < 0) /* sqrt(-n) == -1 (Not Applicable) */
-		return -1;
-	else if (n == 0 || n == 1) /* sqrt(0) == 0, sqrt(1) == 1 */
-		return n;
+	if (n == 1)		/*sqrt(1) == 1*/
+		return (1);
+	else if (n == 0)	/*sqrt(0) == 0*/
+		return (0);
+	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+		return (-1);
 	else
-		return _sqrt_recursion_wrapper(n, 1, n);
+		return (_sqrt_recursion_wrapper(n, 1, n));
 }
